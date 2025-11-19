5dfbccd43cf5bdab7746f2bdc63bd1c1fcbe7e4f
Pardi Pardi <pardi.jhr@gmail.com>
2025-11-19 09:25:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7968430515-d32d0",
+  "appId": "1:929552567620:web:17cfd2ef61f815554b9b11",
+  "apiKey": "AIzaSyABIjdCXF3bunukhrcflhOl1rjMKMLK7Mc",
+  "authDomain": "studio-7968430515-d32d0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "929552567620"
+};

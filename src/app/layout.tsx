0bdbf0e6f09977907b6275b28702e906de122074
Pardi Pardi <pardi.jhr@gmail.com>
2025-11-20 02:49:14 +0000

    Try fixing this error: `Runtime Error: Error: useUser must be used withi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { AppLayout } from '@/components/app/app-layout';
+import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
   title: 'Gudang Kontrak',
@@ -24,7 +25,9 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased">
-        <AppLayout>{children}</AppLayout>
+        <FirebaseClientProvider>
+          <AppLayout>{children}</AppLayout>
+        </FirebaseClientProvider>
         <Toaster />
       </body>
     </html>
--- src/components/app/app-layout.tsx
+++ src/components/app/app-layout.tsx
@@ -3,21 +3,22 @@
 import { AppSidebar } from '@/components/app/app-sidebar';
 import { ContractProvider, useContractContextData } from '@/contexts/contract-context';
 import { ReactNode } from 'react';
-import { FirebaseClientProvider } from '@/firebase/client-provider';
 import FirebaseErrorListener from '../firebase/error-listener';
 
-export function AppLayout({ children }: { children: ReactNode }) {
+function MainContent({ children }: { children: ReactNode }) {
     const contractContextValue = useContractContextData();
 
     return (
-        <FirebaseClientProvider>
-            <ContractProvider value={contractContextValue}>
-                <div className="flex min-h-screen w-full bg-background">
-                    <AppSidebar />
-                    {children}
-                </div>
-                <FirebaseErrorListener />
-            </ContractProvider>
-        </FirebaseClientProvider>
+        <ContractProvider value={contractContextValue}>
+            <div className="flex min-h-screen w-full bg-background">
+                <AppSidebar />
+                {children}
+            </div>
+            <FirebaseErrorListener />
+        </ContractProvider>
     );
+}
+
+export function AppLayout({ children }: { children: ReactNode }) {
+    return <MainContent>{children}</MainContent>;
 }
